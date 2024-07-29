006a3fb655b338fbcdb19d65a1f6610daf508cbb
hafidz gaptex <hafidzmutaali57@gmail.com>
2024-07-29 13:06:58 +0000

    update

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xea65D3f80d2253293a64e2f8DdF4CEC401823053'
-export const ProxyAdmin = '0x6a0baE5CF96d14e39FA9F5AdB4fadABb55B22E17'
-export const SWTRImplementation = '0xE4e93a474734E22A4f660EF1fE7aC3fc096E856c'
+export const SWTRProxy = '0xddD51e0a931E6fc53F21f7A0e8B786184EE4D6B6'
+export const ProxyAdmin = '0x1e17A0fDa66090A7B19757B9954c95e7c01f7afd'
+export const SWTRImplementation = '0x8812703005415C452356060F67D41e2bf03c8C0f'
     
